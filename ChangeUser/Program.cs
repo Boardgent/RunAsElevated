3a52597ffbaad324c9b9cd3f50bb5fdffdb63d02
Ricardo Polo Jaramillo <ricardo.polo@tulpep.com>
2022-08-12 22:11:41 -0500

    Visual studio project added

---
+++ ChangeUser/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProcessStartInfo StartInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                UseShellExecute = false, 
+                WorkingDirectory = Environment.CurrentDirectory,
+                UserName = "user2",
+                Password = MakeSecureString("Passw0rd"),
+                FileName = "ElevatePrivs.exe",
+            };
+
+            Process.Start(StartInfo);
+
+            SecureString MakeSecureString(string password)
+            {
+                SecureString secure = new SecureString();
+                foreach (char c in password)
+                {
+                    secure.AppendChar(c);
+                }
+
+                return secure;
+            }
+        }
+    }
+}
---
+++ ElevatePrivs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatePrivs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo StartInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                UseShellExecute = true, //<- for elevation
+                Verb = "runas",  //<- for elevation
+                WorkingDirectory = Environment.CurrentDirectory,
+                FileName = "cmd.exe",
+                Arguments = "/k whoami /priv"
+            };
+
+            Process.Start(StartInfo);
+        }
+    }
+}
